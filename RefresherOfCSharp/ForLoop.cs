51781a29bafe4042fa5d333d9477dd26c76ac4c1
PatWyzlic <96600690+PatWyzlic@users.noreply.github.com>
2022-02-20 11:22:46 -0500

    Update ForLoop.cs
    Created simple for loop in csharp.

---
+++ RefresherOfCSharp/ForLoop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RefresherOfCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 9; i >= 0; i--)
+            {
+                for(int j = i; j >= 0; j--)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
